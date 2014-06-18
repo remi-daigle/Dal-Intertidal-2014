befe901fbc3a78fd36036381677c5326cd2dd3eb
Remi_D <remi.daigle@dal.ca>
2014-06-18 13:41:06 -0300

    created master figure list

---
+++ Figures.R
@@ -0,0 +1,19 @@
+######## Figure List #############
+getwd()
+ls()
+# site map
+source("SiteMap.R")
+
+
+# Physical variables vs Biological
+# SST
+# Salinity
+# Slope
+# SWH
+# RPD
+# Tidal Height
+# Grain Size
+# Strata
+
+# Derived Biological var (Biodiversity, species richness)
+
