8b0e00e4adb9eb4ad0d373346bd11739e05b4a5f
pamelakrieg <pamela.krieg@dal.ca>
2014-06-19 14:40:25 -0300

    Percent Cover at each strata
    Fixed with titles, all the same y axis scale and

--- Group _A species percent cover vs strata.R
+++ Group _A species percent cover vs strata.R
@@ -4,6 +4,8 @@ rm(list=ls())
 Bio <-read.csv("~/GitHub/Dal-Intertidal-2014/Intertidal_Master_Data_Sheet_2014.csv") 
 require(plyr)
 
+Bio[is.na(Bio)]=0
+
  ### High Tide ###
 table_mean <- ddply(Bio,.(Strata),summarize,
                #QUADRAT SAMPLES#
@@ -11,7 +13,7 @@ table_mean <- ddply(Bio,.(Strata),summarize,
                Fucus_distichous_drift_percentcover=mean(pc_Fucus_distichous_drift,na.rm = TRUE),
                Fucus_distichous_attached_percentcover=mean(pc_Fucus_distichous_attached,na.rm = TRUE),
                Fucus_vesiculosus_percentcover=mean(pc_Fucus_vesiculosus,na.rm = TRUE),
-               Ulva_.something..._percentcover=mean(pc_Ulva_.something...,na.rm = TRUE),  
+               Ulva_.something..._percentcover=mean(pc_Ulva_.something...,na.rm = TRUE)  
               
 )
 table_sd <- ddply(Bio,.(Strata),summarize,
@@ -20,36 +22,35 @@ table_sd <- ddply(Bio,.(Strata),summarize,
                   Fucus_distichous_drift_percentcover=sd(pc_Fucus_distichous_drift,na.rm = TRUE),
                   Fucus_distichous_attached_percentcover=sd(pc_Fucus_distichous_attached,na.rm = TRUE),
                   Fucus_vesiculosus_percentcover=sd(pc_Fucus_vesiculosus,na.rm = TRUE),
-                  Ulva_.something..._percentcover=sd(pc_Ulva_.something...,na.rm = TRUE),
+                  Ulva_.something..._percentcover=sd(pc_Ulva_.something...,na.rm = TRUE)
 )
+
+sp_names=c("Fucus spiralis","Fucus distichus drift","Fucus distichus attached","Fucus vesiculosus","Ulva sp.") 
 
 stdErr <- function(x) {
   x2=x[is.na(x)==F]              # remove the NA's
   sd(x2)/ sqrt(length(x2))       # calculate SE
 }
+
 table_stdErr <- ddply(Bio,.(Strata),summarize,
-                  #QUADRAT SAMPLES#
-                  Mytilus_sp_abunbance=stdErr(ab_m2_Mytilus_sp/Quadrat_m2),
-                  Semibalanus_balanoides_abunbance=stdErr(ab_m2_Semibalanus_balanoides/Quadrat_m2),
-                  Littorina_saxatilis_abunbance=stdErr(ab_m2_Littorina_saxatilis/Quadrat_m2),
-                  Pagurus_longicarpus_abunbance=stdErr(ab_m2_Pagurus_longicarpus/Quadrat_m2),
-                  Carcinus_maenas_abunbance=stdErr(ab_m2_Carcinus_maenas/Quadrat_m2),
-                  ab_m2_Chondrus.crispus_abundance=stdErr(ab_m2_Chondrus.crispus/Quadrat_m2),  
-                  #Core Samples#
-                  Mya_arenaria_abunbance=stdErr(ab_m3_Mya_arenaria/Core_m3),
-                  Mytilus_sp_abunbance=stdErr(ab_m3_Mytilus_sp/Core_m3),
-                  Semibalanus_balanoide_abunbance=stdErr(ab_m3_Semibalanus_balanoides/Core_m3),
-                  Pagurus_longicarpus_abunbance=stdErr(ab_m3_Pagurus_longicarpus/Core_m3),
-                  Glycera_dibrachiata_abunbance=stdErr(ab_m3_Glycera_dibrachiata/Core_m3),
-                  ab_m3_Asterias.spp_abunbance=stdErr(ab_m3_Asterias.spp./Core_m3)
+                      #QUADRAT SAMPLES#
+                      Fucus_spiralis_percentcover=stdErr(pc_Fucus_spiralis),
+                      Fucus_distichous_drift_percentcover=stdErr(pc_Fucus_distichous_drift),
+                      Fucus_distichous_attached_percentcover=stdErr(pc_Fucus_distichous_attached),
+                      Fucus_vesiculosus_percentcover=stdErr(pc_Fucus_vesiculosus),
+                      Ulva_.something..._percentcover=stdErr(pc_Ulva_.something...)                    
 )
 
 
-barplot(table_mean$Mytilus_sp_abunbance[c(1,3,2)], names.arg=c("High","Mid","Low"), xlab="Tidal Ranges",ylab= "Mean abundances")
+
+for(sp in names(table_mean[,-1])){
+  mp <- barplot(table_mean[c(1,3,2),names(table_mean)==sp], names.arg=c("High","Mid","Low"), main=sp_names[names(table_mean[,-1])==sp],xlab="Tidal Ranges",ylab= "Mean percent cover",ylim=c(0,0.4))
+  segments(mp, table_mean[c(1,3,2),names(table_mean)==sp] + table_stdErr[c(1,3,2),names(table_mean)==sp], mp,table_mean[c(1,3,2),names(table_mean)==sp], lwd=2)
+  segments(mp - 0.1, table_mean[c(1,3,2),names(table_mean)==sp] + table_stdErr[c(1,3,2),names(table_mean)==sp], mp + 0.1, table_mean[c(1,3,2),names(table_mean)==sp] + table_stdErr[c(1,3,2),names(table_mean)==sp], lwd=2)
+}
 
 
-
-                  
+                 
   
 obj <- hist(cdata$size_mm[cdata$strata=="H"])
 obj$counts <- obj$counts/sum(obj$counts)*100
