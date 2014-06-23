5ec13380c9c6a7d154004b8f0eed184e2887605e
daniellelmanuel <daniellelmanuel@Danielle-Manuels-MacBook-Pro-2.local>
2014-06-22 22:13:11 -0300

    Slope Vs. Abundance

---
+++ Team B Code/Slope Vs. Abundance.R
@@ -0,0 +1,37 @@
+Slope vs. Abundances (Biological)
+
+#Clear Yer Stuff
+rm(list=ls())
+
+#Load Data
+TotalData<- read.csv("/Users/daniellelmanuel/Desktop/Intertidal_Master_Data_Sheet_2014forR.csv")
+
+######### Make new data set with only Slope and abundances #########
+
+#Take Mean of Abundances per Strata (At each site ~ Slope)
+
+TotalData[is.na(TotalData)]=0 ##Change NA to 0
+
+
+## Means
+TotalData[is.na(TotalData)]=0
+FigureTableSlope<- ddply(TotalData,.(Slope),summarize,
+                       MeanAbundance=mean(ab_m2_Mytilus_sp/Quadrat_m2,na.rm = TRUE),
+                       NAbundance=sum(is.na(ab_m2_Mytilus_sp)==F)
+)
+
+###### Make Graph ########
+
+attach(mtcars)
+
+jpeg('SLOPEAbundanceMytilusm2', height=1200, width=2400, res=400, qual=100)
+plot(FigureTableSlope$Slope,FigureTableSlope$MeanAbundance, main=" ", 
+     xlab="Slope ", ylab="Abundance (Ind/m2) ", pch=19)
+dev.off()
+
+###Add fit lines
+abline(lm(FigureTableSlope$MeanAbundance~FigureTableSlope$Slope), col="red") # regression line (y~x) 
+
+
+getwd()
+
