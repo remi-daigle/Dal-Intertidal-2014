281d68f2a5324214037fb6943bf0b914135c4346
Remi_D <remi.daigle@dal.ca>
2014-06-26 13:58:26 -0300

    fixed biodiversity

--- Richness_A.R
+++ Richness_A.R
@@ -1,38 +1,48 @@
 #import intertidal master data sheet
 
-cdata <- read.csv("~/GitHub/Dal-Intertidal-2014/Intertidal_Master_Data_Sheet_2014.csv")
+cdata <- read.csv(paste(wd,"Intertidal_Master_Data_Sheet_2014.csv",sep=""))
 
 #abundance and species richness
 
 species <- cdata[,substr(names(cdata),1,2)=="ab"|substr(names(cdata),1,2)=="pc"]
 
-test <- as.data.frame(matrix(NA,nrow=length(unique(cdata$Site_name)),ncol=length(names(species))))
-names(test) <- names(species)
-row.names(test) <- unique(cdata$Site_name)
+#make presence/absence matrix
+PA <- as.data.frame(matrix(NA,nrow=length(unique(cdata$Site_name)),ncol=length(names(species))))
+names(PA) <- names(species)
+row.names(PA) <- unique(cdata$Site_name)
 for(site in unique(cdata$Site_name)){
-  for(sp in names(species)){
-    print(site)
-    print(sp)
-    test[unique(cdata$Site_name)==site,names(species)==sp] <- sum(is.na(species[cdata$Site_name==site,names(species)==sp])==F)>=1
+  for(sp in names(PA)){
+    PA[unique(cdata$Site_name)==site,names(species)==sp] <- sum(is.na(species[cdata$Site_name==site,names(species)==sp])==F)>=1
   }
 }
+
+richness <- apply(PA,1,sum)
 
 #next, calculate diversity
 #using abundance (if it starts with ab) sum up each column
 
 #install vegan to calculate diversity
 
-install.packages("vegan")
+#install.packages("vegan")
 require(vegan)
 
+
+species2 <- PA
+for(site in unique(cdata$Site_name)){
+  for(sp in names(species2)){
+    species2[unique(cdata$Site_name)==site,names(species2)==sp] <- mean(species[unique(cdata$Site_name)==site,names(species2)==sp],na.rm=T)
+  }
+}
+
 #create a dataframe that only had abundances
-species <- cdata[,substr(names(cdata),1,2)=="ab"]
+species <- species2[,substr(names(species2),1,2)=="ab"]
+
 
 #made all nas = 0
 species[is.na(species)] <- 0
 
 #runs diversity
-diversity(species, "shannon")
+diversity <- diversity(species, "shannon")
 
 
 
