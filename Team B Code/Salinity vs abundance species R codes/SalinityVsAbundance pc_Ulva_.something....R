960aa55ec340e6b43a031a85bdfc567d0675750f
Feining23 <fn904204@dal.ca>
2014-06-26 00:10:57 -0300

    adding

--- Team B Code/Salinity vs abundance species R codes/SalinityVsAbundance pc_Ulva_.something....R
+++ Team B Code/Salinity vs abundance species R codes/SalinityVsAbundance pc_Ulva_.something....R
@@ -1,10 +1,5 @@
 #Clear Yer Stuff
 rm(list=ls())
-<<<<<<< HEAD
-=======
-#load data
-cdata<- read.csv("~/GitHub/Dal-Intertidal-2014/Intertidal_Master_Data_Sheet_2014.csv")
->>>>>>> origin/master
 
 #Load Data
 TotalData<- read.csv("~/GitHub/Dal-Intertidal-2014/Intertidal_Master_Data_Sheet_2014.csv")
@@ -12,26 +7,26 @@ TotalData<- read.csv("~/GitHub/Dal-Intertidal-2014/Intertidal_Master_Data_Sheet_
 ######### Make new data set with only salinities and abundances#########
 
 #Gives Salinities at high tide
-unique(TotalData$SST[TotalData$Strata=="H"])
+unique(TotalData$Salinity[TotalData$Strata=="H"])
 
 #Bind salinity and abundance in data table
-SSTTable <- cbind(TotalData$SST, TotalData$pc_Ulva_.something...)
+SSTTable <- cbind(TotalData$Salinity, TotalData$pc_Ulva_.something...)
 
 #Makes SalinityTable a new data frame
-SSTTable <- as.data.frame(SSTTable)
+SSTTable <- as.data.frame(SalinityTable)
 
 # change NAs to 0s
-SSTTable$V2[is.na(SSTTable$V2)]=0
+SSTTable$V2[is.na(SalinityTable$V2)]=0
 
 #Change Column Names
 
-colnames(SSTTable) <- c("SST", "Percent Coverage")
+colnames(SalinityTable) <- c("Salinity", "Percent Coverage")
 
 
 ##
 require(plyr)
 TotalData[is.na(TotalData)]=0
-FigureTable<- ddply(TotalData,.(SST),summarize,
+FigureTable<- ddply(TotalData,.(Salinity),summarize,
                     MeanPercentCoverage=mean(pc_Ulva_.something...,na.rm = TRUE),
                     SDPercentCoverage=sd(pc_Ulva_.something...,na.rm = TRUE),
                     NPercentCoverage=sum(is.na(pc_Ulva_.something...)==F)
@@ -39,18 +34,15 @@ FigureTable<- ddply(TotalData,.(SST),summarize,
 
 ###### Make Graph ########
 
-jpeg('SSTPercent Coverage1.jpeg', height=1200, width=2400, res=400, qual=100 )
-barplot(FigureTable$MeanPercentCoverage, names.arg=FigureTable$SST, xlab="SST", ylab= expression ("Percent Coverage (%)"), main=" ")
-dev.off()
-getwd()
+
 
 #### Error Bars ###
 
 AB_mean <- FigureTable$MeanPercentCoverage*100
 AB_se <- FigureTable$SDPercentCoverage/sqrt(FigureTable$NPercentCoverage)*100
 
-jpeg('SSTPercent Coverage1.jpg', height=1200, width=2400, res=400, qual=100 )
-mp <- barplot(AB_mean, names.arg=FigureTable$SST, xlab="SST", ylab= expression ("Percent coverage (%)"), main=" ",ylim=c(0,1)) # plots the barplot and saves the midpoints in mp
+jpeg('SalinityPercent Coverage1.jpg', height=1200, width=2400, res=400, qual=100 )
+mp <- barplot(AB_mean, names.arg=FigureTable$Salinity, xlab="Salinity", ylab= expression ("Percent coverage (%)"), main=" ",ylim=c(0,1)) # plots the barplot and saves the midpoints in mp
 segments(mp, AB_mean + AB_se, mp,AB_mean, lwd=2) # plots positive error bar centered on mp
 segments(mp - 0.1, AB_mean + AB_se, mp + 0.1, AB_mean + AB_se, lwd=2) #plots error bar caps
 dev.off()
