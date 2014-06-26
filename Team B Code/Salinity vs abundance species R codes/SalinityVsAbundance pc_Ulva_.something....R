424edcbf0792293bafedbca12618ce25d8c076d8
Feining23 <fn904204@dal.ca>
2014-06-26 00:25:43 -0300

    adding
    percent coverage VS salinity

---
+++ Team B Code/Salinity vs abundance species R codes/SalinityVsAbundance pc_Ulva_.something....R
@@ -0,0 +1,50 @@
+#Clear Yer Stuff
+rm(list=ls())
+
+#Load Data
+TotalData<- read.csv("~/GitHub/Dal-Intertidal-2014/Intertidal_Master_Data_Sheet_2014.csv")
+
+######### Make new data set with only salinities and abundances#########
+
+#Gives Salinities at high tide
+unique(TotalData$Salinity[TotalData$Strata=="H"])
+
+#Bind salinity and abundance in data table
+SSTTable <- cbind(TotalData$Salinity, TotalData$pc_Ulva_.something...)
+
+#Makes SalinityTable a new data frame
+SSTTable <- as.data.frame(SalinityTable)
+
+# change NAs to 0s
+SSTTable$V2[is.na(SalinityTable$V2)]=0
+
+#Change Column Names
+
+colnames(SalinityTable) <- c("Salinity", "Percent Coverage")
+
+
+##
+require(plyr)
+TotalData[is.na(TotalData)]=0
+FigureTable<- ddply(TotalData,.(Salinity),summarize,
+                    MeanPercentCoverage=mean(pc_Ulva_.something...,na.rm = TRUE),
+                    SDPercentCoverage=sd(pc_Ulva_.something...,na.rm = TRUE),
+                    NPercentCoverage=sum(is.na(pc_Ulva_.something...)==F)
+)
+
+###### Make Graph ########
+
+
+
+#### Error Bars ###
+
+AB_mean <- FigureTable$MeanPercentCoverage*100
+AB_se <- FigureTable$SDPercentCoverage/sqrt(FigureTable$NPercentCoverage)*100
+
+jpeg('SalinityPercent Coverage1.jpg', height=1200, width=2400, res=400, qual=100 )
+mp <- barplot(AB_mean, names.arg=FigureTable$Salinity, xlab="Salinity", ylab= expression ("Percent coverage (%)"), main=" ",ylim=c(0,50)) # plots the barplot and saves the midpoints in mp
+segments(mp, AB_mean + AB_se, mp,AB_mean, lwd=2) # plots positive error bar centered on mp
+segments(mp - 0.1, AB_mean + AB_se, mp + 0.1, AB_mean + AB_se, lwd=2) #plots error bar caps
+dev.off()
+
+getwd()
