3e9ce8facbc945096eca1c22761dbe704ae72bd6
arlie1991 <ar730339@dal.ca>
2014-06-26 14:42:43 -0300

    Slope codes
    pictures aren't right yet - slope data wasn't added.

---
+++ Team B Code/Slope vs. Abundance/Slope - ab_m3_Mytilus_sp.R
@@ -0,0 +1,40 @@
+Slope vs. Abundances (Biological)
+
+#Clear Yer Stuff
+rm(list=ls())
+
+
+#Load Data
+TotalData<- read.csv(paste(wd, "Intertidal_Master_Data_Sheet_2014.csv", sep=""))
+
+**Be sure not to clear the loaded data after setting the directory
+
+
+######### Make new data set with only Slope and abundances #########
+
+#Take Mean of Abundances per Strata (At each site ~ Slope)
+
+TotalData[is.na(TotalData)]=0 ##Change NA to 0
+
+
+## Means
+TotalData[is.na(TotalData)]=0
+FigureTableSlope<- ddply(TotalData,.(Slope),summarize,
+                         MeanAbundance=mean(ab_m3_Mytilus_sp/Core_m3,na.rm = TRUE),
+                         NAbundance=sum(is.na(ab_m3_Mytilus_sp)==F)
+)
+
+###### Make Graph ########
+
+attach(mtcars)
+
+jpeg('SLOPEAbundanceMytilusSp(m3).jpg', height=1200, width=2400, res=400, qual=100)
+plot(FigureTableSlope$Slope,FigureTableSlope$MeanAbundance, main=" ",
+     xlab="Slope ", ylab="Abundance (Ind/m3) ", pch=19)
+dev.off()
+
+###Add fit lines
+abline(lm(FigureTableSlope$MeanAbundance~FigureTableSlope$Slope), col="red") # regression line (y~x)
+
+
+getwd()
