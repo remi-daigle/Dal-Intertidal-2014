f791bc1a6f4457a92fa8a797fabe2e0733e73eb4
Remi_D <remi.daigle@dal.ca>
2014-06-26 22:31:55 -0300

    edits to richness to plot against physical var.
    barplots are not the appropriate plot, should be just a standard x/y
    plot. This needs tweaking before use in your report, but at least you
    can see some patterns

--- Richness_A.R
+++ Richness_A.R
@@ -16,7 +16,7 @@ for(site in unique(cdata$Site_name)){
   }
 }
 
-richness <- apply(PA,1,sum)
+rich <- apply(PA,1,sum)
 
 #next, calculate diversity
 #using abundance (if it starts with ab) sum up each column
@@ -40,10 +40,63 @@ species <- species2[,substr(names(species2),1,2)=="ab"]
 
 #made all nas = 0
 species[is.na(species)] <- 0
+cdata[is.na(cdata)] <- 0
 
 #runs diversity
-diversity <- diversity(species, "shannon")
+div <- diversity(species, "shannon")
+
+# make data frame
+df <- as.data.frame(cbind(div=div,rich=rich))
+df$Site <- row.names(df)
 
 
+# merge with main dataframe
+cdata2 <- merge(cdata,df,by.x="Site_name",by.y="Site")
+
+# break up data
+phys <- cdata2[,9:21]
 
 
+# make vector of label
+## TO DO: rename labels!
+phys_names <- c("Slope","SST","Temp_Buoy","Salinity","Salt_Buoy","RPD","SWH","Tidal_Average",
+                "Tidal_Range","X..0.5.mm","X..1.7.mm","X..11.2.mm","X..11.2.mm.1")
+
+## calculate means and plot
+require(plyr)
+
+# for richness
+for(ph in names(phys)){
+    df=as.data.frame(cbind(x=phys[,names(phys)==ph],y=cdata2$rich))
+    FigureTable<- ddply(df,.(x),summarize,
+                        MeanAbundance=mean(y,na.rm = TRUE),
+                        SDAbundance=sd(y,na.rm = TRUE),
+                        NAbundance=sum(is.na(y)==F)
+    )
+    AB_mean <- FigureTable$MeanAbundance
+    AB_se <- FigureTable$SDAbundance/sqrt(FigureTable$NAbundance)
+    jpeg(paste("Richness_",ph,".jpg"), height=1200, width=2400, res=400, qual=100  )
+    if(is.nan(max(AB_mean)*2)==F) mp <- barplot(FigureTable$MeanAbundance, names.arg=FigureTable$x, xlab=phys_names[names(phys)==ph], ylab= "Richness",ylim=c(0,max(AB_mean)*2))              # plots the barplot and saves the midpoints in mp
+    if(is.nan(max(AB_mean)*2)==T) mp <- barplot(FigureTable$MeanAbundance, names.arg=FigureTable$x, xlab=phys_names[names(phys)==ph], ylab= "Richness",ylim=c(0,1))              # plots the barplot and saves the midpoints in mp
+    segments(mp, AB_mean + AB_se, mp,AB_mean, lwd=2)  # plots positive error bar centered on mp
+    segments(mp - 0.1, AB_mean + AB_se, mp + 0.1, AB_mean + AB_se, lwd=2)  #plots error bar caps
+    dev.off()
+}
+
+#for diversity
+for(ph in names(phys)){
+  df=as.data.frame(cbind(x=phys[,names(phys)==ph],y=cdata2$div))
+  FigureTable<- ddply(df,.(x),summarize,
+                      MeanAbundance=mean(y,na.rm = TRUE),
+                      SDAbundance=sd(y,na.rm = TRUE),
+                      NAbundance=sum(is.na(y)==F)
+  )
+  AB_mean <- FigureTable$MeanAbundance
+  AB_se <- FigureTable$SDAbundance/sqrt(FigureTable$NAbundance)
+  jpeg(paste("Biodiversity_",ph,".jpg"), height=1200, width=2400, res=400, qual=100  )
+  if(is.nan(max(AB_mean)*2)==F) mp <- barplot(FigureTable$MeanAbundance, names.arg=FigureTable$x, xlab=phys_names[names(phys)==ph], ylab= "Biodiversity",ylim=c(0,max(AB_mean)*2))              # plots the barplot and saves the midpoints in mp
+  if(is.nan(max(AB_mean)*2)==T) mp <- barplot(FigureTable$MeanAbundance, names.arg=FigureTable$x, xlab=phys_names[names(phys)==ph], ylab= "Biodiversity",ylim=c(0,1))              # plots the barplot and saves the midpoints in mp
+  segments(mp, AB_mean + AB_se, mp,AB_mean, lwd=2)  # plots positive error bar centered on mp
+  segments(mp - 0.1, AB_mean + AB_se, mp + 0.1, AB_mean + AB_se, lwd=2)  #plots error bar caps
+  dev.off()
+}
